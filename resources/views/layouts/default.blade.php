b75fe5bbd40aece60d31cafe18fddb06523a3a95
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2022-01-15 11:11:43 +0100

    create new layout

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CSS Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <!-- JS Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+
+    <title>@yield("page_title")</title>
+</head>
+
+<body>
+
+    <header>
+        @yield("header_content")
+    </header>
+
+    <main>
+        @yield("main_content")
+    </main>
+
+    <footer>
+        @yield("footer_content")
+    </footer>
+
+</body>
+
+</html>
