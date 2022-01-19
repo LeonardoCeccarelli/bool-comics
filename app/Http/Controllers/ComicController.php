26cac03f8c20ac8018bcc4829c89e197261473bc
alfredochs <chanta.alfredo@gmail.com>
2022-01-19 14:47:32 +0100

    reset controller

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -57,7 +57,7 @@ class ComicController extends Controller
      */
     public function edit(Comic $comic)
     {
-        return view('comics.edit',compact('comic'));
+
     }
 
     /**
@@ -69,9 +69,7 @@ class ComicController extends Controller
      */
     public function update(Request $request, Comic $comic)
     {
-        $data = $request->all();
-        $comic->update($data);
-        return redirect()->route('comics.show',$comic->id);
+        
     }
 
     /**
