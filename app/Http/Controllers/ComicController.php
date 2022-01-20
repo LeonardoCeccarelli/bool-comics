23d1f74a5e3446e08d50298bf15ad3e91c9e27c8
Eddishot <Eddi_sl94@hotmail.it>
2022-01-20 17:14:19 +0100

    reset index

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -15,6 +15,7 @@ class ComicController extends Controller
     public function index()
     {
         $comics= Comic::all();
+        
         return view("comics.index", compact("comics"));
     }
 
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('comics.index');
-// });
 Route::get('/', function () {
 
     return redirect()->route("comics.index");
