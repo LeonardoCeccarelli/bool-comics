3b21089a1ae2942be00183cfc6fe749aa2f49354
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2022-01-22 16:23:40 +0100

    change property in line 15 in the_navbar

--- resources/views/partials/the_navbar.blade.php
+++ resources/views/partials/the_navbar.blade.php
@@ -12,7 +12,7 @@
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-bs-toggle="dropdown" aria-expanded="false">
+                        data-toggle="dropdown" aria-expanded="false">
                         Comics
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
